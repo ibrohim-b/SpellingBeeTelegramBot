09363be52f38292a65e36e65a4bf85defd366a2e
Ibrohim Bahritidinov <111245865+ibrohim-b@users.noreply.github.com>
2024-04-20 15:10:03 +0500

    Update main.py

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ async def spelling_a_word(message: types.Message, state: FSMContext):
         await message.answer(
             text=messages_config["incorrect_spelling_message"].format(word=user_word.word_spell), parse_mode="HTML",
             reply_markup=keyboard_markup)
-        await state.set_state(UserState.spelling_a_word)
+        await state.set_state(UserState.picking_new_word)
 
 
 @dp.message(Command("stats") or (("stats" or "Stats") and not StateFilter(UserState.spelling_a_word)))
