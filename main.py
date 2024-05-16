4ade52c53f80386e128a511ef49d33c5ac531e05
Ibrohim Bahritidinov <ibo.bahritidinov@gmail.com>
2024-05-16 10:40:12 +0500

    getting ready to merge to the main

--- main.py
+++ main.py
@@ -10,12 +10,10 @@ from aiogram.filters.command import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.types import FSInputFile
+from aiogram.types import FSInputFile, InputFile
 from dotenv import load_dotenv
 
-from Word import Word
-from db_sdk import DatabaseRepository
-from dictionary_sdk import DictionaryClient, DictionaryWord
+from spelling_bee_api_sdk import Word, SpellingBeeSdk
 
 setproctitle.setproctitle("SpellingBeeBot")
 
@@ -27,8 +25,7 @@ load_dotenv()
 BOT_TOKEN = os.getenv('api_key')
 storage = MemoryStorage()
 
-dictionary_sdk = DictionaryClient()
-db_sdk = DatabaseRepository()
+spelling_bee_sdk = SpellingBeeSdk()
 
 logging.basicConfig(level=logging.INFO, filename="bot.log", filemode="w",
                     format="%(asctime)s %(levelname)s %(message)s")
@@ -48,36 +45,46 @@ class UserState(StatesGroup):
     start_training = State()
     spelling_a_word = State()
     picking_new_word = State()
-    lastWord = Word("", "", "")
+    lastWord = Word(0, "", "", None)
 
 
 # Handle /start command
 @dp.message(Command("start"))
 async def authorize(message: types.Message, state: FSMContext):
-    if db_sdk.user_exists(message.from_user.id) or db_sdk.user_has_name(message.from_user.id):
-        await state.set_state(UserState.authorized)
-        await start(message)
-    else:
-        await state.set_state(UserState.registration)
-        await registration(message, state)
+    try:
+        if spelling_bee_sdk.user_exists(message.from_user.id) or spelling_bee_sdk.user_has_name(message.from_user.id):
+            await state.set_state(UserState.authorized)
+            await start(message)
+        else:
+            await state.set_state(UserState.registration)
+            await registration(message, state)
+    except Exception:
+        await something_went_wrong(message)
 
 
 async def registration(message: types.Message, state: FSMContext):
-    if not db_sdk.user_exists(message.from_user.id) or not db_sdk.user_has_name(message.from_user.id):
-        await message.answer(
-            text=messages_config['registration_message'],
-        )
-        await state.set_state(UserState.entering_name)
+    try:
+        if not spelling_bee_sdk.user_exists(message.from_user.id) or not spelling_bee_sdk.user_has_name(
+                message.from_user.id):
+            await message.answer(
+                text=messages_config['registration_message'],
+            )
+            await state.set_state(UserState.entering_name)
+    except Exception:
+        await something_went_wrong(message)
 
 
 @dp.message(StateFilter(UserState.entering_name))
 async def entering_name(message: types.Message, state: FSMContext):
-    if not db_sdk.user_exists(message.from_user.id):
-        db_sdk.create_user(message.from_user.id, message.text)
-    elif not db_sdk.user_has_name(message.from_user.id):
-        db_sdk.update_user_name(message.from_user.id, message.text)
-    await state.set_state(UserState.authorized)
-    await start(message)
+    try:
+        if not spelling_bee_sdk.user_exists(message.from_user.id):
+            spelling_bee_sdk.create_user(message.from_user.id, message.text)
+        elif not spelling_bee_sdk.user_has_name(message.from_user.id):
+            spelling_bee_sdk.update_user_name(message.from_user.id, message.text)
+        await state.set_state(UserState.authorized)
+        await start(message)
+    except Exception:
+        await something_went_wrong(message)
 
 
 async def start(message: types.Message):
@@ -92,7 +99,7 @@ async def start(message: types.Message):
     )
 
 
-@dp.message(Command("cancel"))
+@dp.message(Command("cancel") and StateFilter(UserState.authorized))
 async def cancel(message: types.Message, state: FSMContext):
     await state.set_state(None)
     await message.answer(
@@ -108,34 +115,39 @@ async def pick_a_word(callback: types.CallbackQuery, state: FSMContext):
 
 @dp.message(StateFilter(UserState.picking_new_word) or StateFilter(UserState.spelling_a_word))
 async def pick_a_word(message: types.Message, state: FSMContext):
-    word: Word = db_sdk.get_random_word(str(message.from_user.id))
-    dictionary_word: DictionaryWord | None = dictionary_sdk.get_definition(word.word_spell)
-    audio = FSInputFile(path=f"words_mp3/{word.word_spell}.mp3")
-    translation = word.word_translation
-    if dictionary_word:
-        phonetics_list = ", ".join(dictionary_word.phonetics) if dictionary_word.phonetics else "No phonetics available"
-        def_obj = dictionary_word.meanings[0].definitions[0]
-        definition = def_obj.definition if def_obj.definition else "No definition available"
-        example_sentence = def_obj.example.replace(word.word_spell,
-                                                   "___") if def_obj.example else "No example sentence available"
-        synonyms_list = ", ".join(dictionary_word.meanings[0].synonyms) if dictionary_word.meanings[
-            0].synonyms else "No synonyms available"
-        antonyms_list = ", ".join(dictionary_word.meanings[0].antonyms) if dictionary_word.meanings[
-            0].antonyms else "No antonyms available"
-    else:
-        phonetics_list = "No phonetics available"
-        definition = "No definition available"
-        example_sentence = "No example sentence available"
-        synonyms_list = "No synonyms available"
-        antonyms_list = "No antonyms available"
-    caption = messages_config['word_message'].format(translation=translation, phonetics_list=phonetics_list,
-                                                     definition=definition,
-                                                     example_sentence=example_sentence,
-                                                     synonyms_list=synonyms_list,
-                                                     antonyms_list=antonyms_list)
-    await bot.send_voice(message.chat.id, audio, caption=caption, reply_markup=types.ReplyKeyboardRemove())
-    await state.set_state(UserState.spelling_a_word)
-    await state.update_data(lastWord=word)
+    try:
+        word: Word = spelling_bee_sdk.get_random_word(message.from_user.id)
+        extra_info = word.extra_info
+        translation = word.word_translation
+        audio: InputFile
+        if extra_info:
+            audio = FSInputFile(f"words_mp3/{word.word_spell}.mp3")
+            phonetics_list = ", ".join(extra_info.phonetics) if extra_info.phonetics else "No phonetics available"
+            def_obj = extra_info.meanings[0].definitions[0]
+            definition = def_obj.definition if def_obj.definition else "No definition available"
+            example_sentence = def_obj.example.replace(word.word_spell,
+                                                       "___") if def_obj.example else "No example sentence available"
+            synonyms_list = ", ".join(extra_info.meanings[0].synonyms) if extra_info.meanings[
+                0].synonyms else "No synonyms available"
+            antonyms_list = ", ".join(extra_info.meanings[0].antonyms) if extra_info.meanings[
+                0].antonyms else "No antonyms available"
+        else:
+            audio = FSInputFile(f"words_mp3/{word.word_spell}.mp3")
+            phonetics_list = "No phonetics available"
+            definition = "No definition available"
+            example_sentence = "No example sentence available"
+            synonyms_list = "No synonyms available"
+            antonyms_list = "No antonyms available"
+        caption = messages_config['word_message'].format(translation=translation, phonetics_list=phonetics_list,
+                                                         definition=definition,
+                                                         example_sentence=example_sentence,
+                                                         synonyms_list=synonyms_list,
+                                                         antonyms_list=antonyms_list)
+        await bot.send_voice(message.chat.id, audio, caption=caption, reply_markup=types.ReplyKeyboardRemove())
+        await state.set_state(UserState.spelling_a_word)
+        await state.update_data(lastWord=word)
+    except Exception:
+        await something_went_wrong(message)
 
 
 @dp.message(StateFilter(UserState.spelling_a_word))
@@ -145,10 +157,10 @@ async def spelling_a_word(message: types.Message, state: FSMContext):
     user_word: Word = user_data['lastWord']
     logging.info(f"User {message.from_user.id} tries to pick a {user_word}.")
 
-    db_sdk.add_suggestion(user_word.word_id, user_id)
+    spelling_bee_sdk.add_suggestion(user_word.word_id, user_id)
 
     if message.text.lower().strip() == user_word.word_spell.lower().strip():
-        db_sdk.update_suggestion(user_word.word_id, user_id, 1)
+        spelling_bee_sdk.update_suggestion(user_word.word_id, user_id, 1)
         logging.info(f"User {message.from_user.id} picked up {user_word} correctly.")
         await message.answer(
             text=messages_config["congratulations_message"], )
@@ -165,16 +177,26 @@ async def spelling_a_word(message: types.Message, state: FSMContext):
 
 @dp.callback_query(F.data == "statistics")
 async def stats(callback: types.CallbackQuery):
-    message = callback.message
-    user_id = message.from_user.id
-    passed_words = db_sdk.get_total_words_passed_count(user_id)
-    total_words = db_sdk.get_total_words_count()
-    total_words_left = total_words - passed_words
-    logging.info(f"User {message.from_user.id} requested stats.")
+    try:
+        message = callback.message
+        user_id = message.from_user.id
+        passed_words = spelling_bee_sdk.get_total_words_passed_count(user_id)
+        total_words = spelling_bee_sdk.get_total_words_count()
+        total_words_left = total_words - passed_words
+        logging.info(f"User {message.from_user.id} requested stats.")
+        await message.answer(
+            text=messages_config["stats_message"].format(passed_words_count=passed_words, total_words_count=total_words,
+                                                         total_words_left=total_words_left),
+            parse_mode="HTML")
+    except Exception:
+        await something_went_wrong(callback.message)
+
+
+@dp.message()
+async def something_went_wrong(message: types.Message):
     await message.answer(
-        text=messages_config["stats_message"].format(passed_words_count=passed_words, total_words_count=total_words,
-                                                     total_words_left=total_words_left),
-        parse_mode="HTML")
+        text=messages_config["something_went_wrong"],
+    )
 
 
 async def main():
---
+++ spelling_bee_api_sdk.py
@@ -0,0 +1,108 @@
+import os
+from dataclasses import dataclass
+from typing import Optional
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class SpellingBeeSdk:
+    def __init__(self):
+        self.base_url = os.getenv("spelling_bee_api_address")
+
+    def get_random_word(self, user_id: int):
+        response = requests.get(f"{self.base_url}/words/get_random_word?user_id={user_id}")
+        if response.status_code == 200:
+            return Word(**response.json())
+        else:
+            return None
+
+    def user_exists(self, user_id: int):
+        response = requests.get(f"{self.base_url}/users/user_exists?user_id={user_id}")
+        if response.status_code == 200:
+            return bool(response.json())
+        return False
+
+    def user_has_name(self, user_id: int):
+        response = requests.get(f"{self.base_url}/users/user_has_name?user_id={user_id}")
+        if response.status_code == 200:
+            return bool(response.json())
+        return False
+
+    def create_user(self, user_id: int, user_name: str):
+        requests.post(f'{self.base_url}/users/create_user?user_id={user_id}&user_name={user_name}')
+
+    def update_user_name(self, user_id: int, user_name: str):
+        requests.put(f'{self.base_url}/users/update_name?user_id={user_id}&user_name={user_name}')
+
+    def add_suggestion(self, word_id: int, user_id: int):
+        requests.put(f'{self.base_url}/suggestions/add_suggestion?word_id={word_id}&user_id={user_id}')
+
+    def update_suggestion(self, word_id: int, user_id: int, passed: int):
+        requests.put(
+            f"{self.base_url}/suggestions/update_suggestion?word_id={word_id}&user_id={user_id}&passed={passed}")
+
+    def get_total_words_passed_count(self, user_id: int):
+        response = requests.get(f"{self.base_url}/statistics/get_total_words_passed_count?user_id={user_id}")
+        if response.status_code == 200:
+            return int(response.json())
+        return 0
+
+    def get_total_words_count(self):
+        response = requests.get(f"{self.base_url}/statistics/get_total_words_count")
+        if response.status_code == 200:
+            return int(response.json())
+        return 0
+
+
+@dataclass
+class Definition:
+    definition: str
+    example: Optional[str]
+
+
+@dataclass
+class Meaning:
+    part_of_speech: str
+    definitions: [Definition]
+    synonyms: [str]
+    antonyms: [str]
+
+    def __post_init__(self):
+        self.definitions = [Definition(**definition) for definition in self.definitions]
+
+
+@dataclass
+class ExtraInfo:
+    phonetics: [str]
+    audio: Optional[str]
+    meanings: [Meaning]
+
+    def __post_init__(self):
+        self.meanings = [Meaning(**meaning) for meaning in self.meanings]
+
+@dataclass
+class Word:
+    word_id: int
+    word_spell: str
+    word_translation: str
+    extra_info: ExtraInfo | None
+
+    def __str__(self):
+        return f"Word(word_id = {self.word_id}, word = {self.word_spell}, word_translation = {self.word_translation})"
+
+    def __post_init__(self):
+        self.extra_info = ExtraInfo(**self.extra_info) if self.extra_info else None
+
+
+if __name__ == '__main__':
+    spelling_bee_api_sdk = SpellingBeeSdk()
+    print(spelling_bee_api_sdk.user_exists(142264444))
+    r = spelling_bee_api_sdk.get_random_word(142264444)
+    print(r)
+    print(r.extra_info)
+    # print(r.extra_info.audio)
+    # print(r.extra_info.phonetics)
+    # print(r.extra_info.meanings[0].part_of_speech)
